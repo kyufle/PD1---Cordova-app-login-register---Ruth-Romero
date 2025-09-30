01f4e755fa9b04066c231467024ede777b12366f
Ruth Romero <ruth.102004@gmail.com>
2025-09-30 12:30:45 +0200

    insert code in cordova project

---
+++ www/js/index.js
@@ -0,0 +1,65 @@
+const usersAndPassword = {
+    ieti : {
+        correo: '',
+        password: 'cordova'
+    },
+};
+
+function fer_login(){
+    const usuari = document.getElementById("usuari").value;
+    const contrasenya = document.getElementById("contrasenya").value;
+    const loginP = document.getElementById("loginP");
+    let login = false;
+    for (users in usersAndPassword){
+        if (users === usuari && usersAndPassword[users].password === contrasenya){
+            login = true;
+        }
+    }
+    if (login){
+        loginP.textContent = "The user and password are correct";
+        loginP.classList.remove("incorrect");
+        loginP.classList.add("correct");
+        alert("Login exitós");
+        
+    } else{
+        loginP.textContent = "The user and password are incorrect";
+        loginP.classList.remove("correct");
+        loginP.classList.add("incorrect");
+        alert("Login incorrecte");
+    }
+}
+
+function registra(){
+    const reg_usuari  = document.getElementById("reg_usuari").value;
+    const reg_email = document.getElementById("reg_email").value;
+    const reg_contrasenya_1 = document.getElementById("reg_contrasenya_1").value;
+    const reg_contrasenya_2 = document.getElementById("reg_contrasenya_2").value;
+    let existente = false;
+    for (users in usersAndPassword){
+        if (reg_usuari === users || reg_email === usersAndPassword[users].correo){
+            existente = true;
+            alert("El nom o l'email ja existeix a la base de dades");
+            signInP.textContent = "The name or email already exists in the database.";
+            signInP.classList.remove("correct");
+            signInP.classList.add("incorrect");
+        }
+    }
+    if (reg_contrasenya_1 != reg_contrasenya_2){
+        alert("Error en les contrasenyes");
+        signInP.textContent = "Password error.";
+        signInP.classList.remove("correct");
+        signInP.classList.add("incorrect");
+        
+    }
+    if (!existente && reg_contrasenya_1 === reg_contrasenya_2){
+        usersAndPassword[reg_usuari] = {
+            correo: reg_email,
+            password: reg_contrasenya_1
+        }
+        alert("Nou usuari registrat correctament");
+        signInP.textContent = "Successfully created.";
+        signInP.classList.remove("incorrect");
+        signInP.classList.add("correct");
+        
+    }
+}
